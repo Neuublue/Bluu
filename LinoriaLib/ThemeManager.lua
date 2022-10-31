afcf52ad8e28bfc44369b74d06d5870476153dc0
de_Neuublue <88302612+Neuublue@users.noreply.github.com>
2022-10-30 22:54:32 -0400

    Add files via upload

--- LinoriaLib/ThemeManager.lua
+++ LinoriaLib/ThemeManager.lua
@@ -6,12 +6,12 @@ local ThemeManager = {} do
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
 		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"0055ff","BackgroundColor":"141414","OutlineColor":"323232"}') },
-		['Green'] 			= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"141414","AccentColor":"00ff8b","BackgroundColor":"1c1c1c","OutlineColor":"3c3c3c"}') },
-		['Jester'] 			= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
-		['Mint'] 			= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"3db488","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
+		['Green'] 			= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"00ff8b","BackgroundColor":"141414","OutlineColor":"323232"}') },
+		['Red'] 			= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"ff4141","BackgroundColor":"141414","OutlineColor":"323232"}') },
 		['Tokyo Night'] 	= { 5, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"323232"}') },
 		['Ubuntu'] 			= { 6, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"3e3e3e","AccentColor":"e2581e","BackgroundColor":"323232","OutlineColor":"191919"}') },
-		['Discord'] 		= { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"2f3136","AccentColor":"5865f2","BackgroundColor":"202225","OutlineColor":"36393f"}') },
+		['Discord'] 		= { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"2f3136","AccentColor":"5865f2","BackgroundColor":"36393f","OutlineColor":"2f3136"}') },
+		['Discord Light']	= { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"f2f3f5","AccentColor":"5865f2","BackgroundColor":"ffffff","OutlineColor":"f2f3f5"}') },
 		['Visual Studio'] 	= { 8, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"252526","AccentColor":"007acc","BackgroundColor":"1e1e1e","OutlineColor":"333333"}') }
 	}
 
